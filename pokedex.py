dad2d99feec0ba94ca8c4822c9db1b33c3821a59
chriscoliveira <christian.coliveira@gmail.com>
2022-09-11 09:51:12 -0300

    add imagens

--- pokedex.py
+++ pokedex.py
@@ -10,14 +10,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('pokedex_home.html')
 
 
 @app.route('/buscar', methods=['GET', 'post'])
 def buscar():
-    pokemon = Pokemon(request.form['nome'].lower(),
-                      '', '', '', '', '', '', '', '', '', '')
+
     try:
+        pokemon = Pokemon(request.form['nome'].lower(),
+                          '', '', '', '', '', '', '', '', '', '')
         res = json.loads(requests.get(
             f'https://pokeapi.co/api/v2/pokemon/{pokemon.nome}').text)
 
@@ -32,14 +33,22 @@ def buscar():
         tipos = []
         for i in range(qtd_tipos):
             tipos.append(res['types'][i]['type']['name'])
-        pokemon.tipo = tipos
+
+        txt = ''
+        for i in tipos:
+            txt += i + ', '
+        pokemon.tipo = txt
 
         # movimentos
         qtd_movimentos = len(res['moves'])
         movimentos = []
         for i in range(qtd_movimentos):
             movimentos.append(res['moves'][i]['move']['name'])
-        pokemon.movimentos = movimentos
+
+        txt = ''
+        for i in movimentos:
+            txt += i + ', '
+        pokemon.movimentos = txt
 
         # peso
         pokemon.peso = res['weight']
@@ -55,7 +64,11 @@ def buscar():
         habilidades = []
         for i in range(qtd_habilidades):
             habilidades.append(res['abilities'][i]['ability']['name'])
-        pokemon.habilidades = habilidades
+
+        txt = ''
+        for i in habilidades:
+            txt += i + ', '
+        pokemon.habilidades = txt
 
         # local
         pokemon.local = res['location_area_encounters']
@@ -65,23 +78,26 @@ def buscar():
         status = []
         for i in range(qtd_status):
             status.append(
-                f"{res['stats'][i]['stat']['name'] } -> {res['stats'][i]['base_stat']}")
-        pokemon.status = status
+                f"{res['stats'][i]['stat']['name'] }={res['stats'][i]['base_stat']} ")
 
+        txt = ''
+        for i in status:
+            txt += i + ', '
+        pokemon.status = txt
+        return render_template('pokedex.html',
+                               nome=pokemon.nome.upper(),
+                               foto=pokemon.foto,
+                               foto1=pokemon.foto1,
+                               tipo=pokemon.tipo.upper(),
+                               movimentos=pokemon.movimentos.upper(),
+                               id=pokemon.id,
+                               peso=pokemon.peso,
+                               habilidades=pokemon.habilidades.upper(),
+                               local=pokemon.local.upper(),
+                               status=pokemon.status.upper(),
+                               formas=pokemon.formas.upper())
     except Exception as e:
-        return 'Pokemon não encontrado '+str(e)
-    return render_template('index.html',
-                           nome=pokemon.nome,
-                           foto=pokemon.foto,
-                           foto1=pokemon.foto1,
-                           tipo=pokemon.tipo,
-                           movimentos=pokemon.movimentos,
-                           id=pokemon.id,
-                           peso=pokemon.peso,
-                           habilidades=pokemon.habilidades,
-                           local=pokemon.local,
-                           status=pokemon.status,
-                           formas=pokemon.formas)
+        return render_template('pokedex_home.html', nome='Pokemon não encontrado', foto='', foto1='', tipo='', movimentos='', id='', peso='', habilidades='', local='', status='', formas='')
 
 
 if __name__ == '__main__':
